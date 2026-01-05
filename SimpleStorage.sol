9452c9f24873e7332b9f0adb84c402df01948d48
Aaditya Bhatt <aaditya_b@ma.iitr.ac.in>
2026-01-05 20:05:11 +0530

    remix ide file

---
+++ SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage {
+    uint256 private storedNumber;
+
+    function set(uint256 _number) public {
+        storedNumber = _number;
+    }
+
+    function get() public view returns (uint256) {
+        return storedNumber;
+    }
+}
